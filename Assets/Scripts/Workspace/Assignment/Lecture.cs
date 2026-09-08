319431dc10a5d1c1a0ddab9f6a887fdf1b1e7818
Jellybeam04 <phittipong040506@gmail.com>
2026-09-08 11:06:46 +0700

    GI262 Week04 - (Class)

--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -22,6 +22,47 @@ namespace Assignment
 
         public void LCT02_SyntaxLinkedList()
         {
+            //string[] playerName = new string[20];
+            LinkedList<string> linkedlist = new LinkedList<string>();
+
+
+            linkedlist.AddLast("Node 1");
+
+
+            linkedlist.AddLast("Node 2");
+
+
+            linkedlist.AddFirst("Node 0");
+
+            LinkedListNode<string> FirstNode = linkedlist.First;
+            Debug.Log("first: " + FirstNode.Value);
+
+            LinkedListNode<string> lastNode = linkedlist.First;
+            Debug.Log("Last: " + lastNode.Value);
+
+
+            Debug.Log("firstNode.Next: " + FirstNode.Next.Value);
+            Debug.Log("firstNode.Next.Next: " + FirstNode.Next.Next.Value);
+
+            Debug.Log("lastNode.Previous: " + lastNode.Previous.Value);
+            Debug.Log("lastNode.Previous.Previous: " + lastNode.Previous.Previous.Value);
+
+            if (FirstNode.Previous == null) Debug.Log("firstNode.Previous == null");
+            if (lastNode.Next == null) Debug.Log("lastNode.Next == null");
+
+            linkedlist.AddAfter(FirstNode, "Node 0.5");
+
+            linkedlist.AddAfter(lastNode, "Node 1.5");
+
+            LinkedListNode<string> node1 = linkedlist.Find("Node 1");
+
+            linkedlist.Remove("Node 1");
+            linkedlist.Remove(node1);
+            linkedlist.RemoveLast();
+            linkedlist.RemoveFirst();
+
+            linkedlist.Clear();
+
             throw new System.NotImplementedException();
         }
 
@@ -32,6 +73,44 @@ namespace Assignment
 
         public void LCT04_SyntaxDictionary()
         {
+            Dictionary<string, int> inv = new Dictionary<string, int>();
+            var inv2 = new Dictionary<string, int>();
+
+
+            inv.Add("Potion", 1);
+
+            inv.Add("Apple", 10);
+
+            inv.Add("Banana", 5);
+
+
+            inv["Potion"] = 10;
+
+            var pickupitem = "Sword";
+            inv[pickupitem] = 1;
+
+            foreach(var pair in inv)
+            {
+                string key = pair.Key;
+                int value = pair.Value;
+                Debug.Log($"Key: {key} value: {value}");
+            }
+
+            var appleExists = inv.ContainsKey("Apple");
+            Debug.Log(appleExists);
+
+            var keyExists = inv.ContainsKey("Key");
+            Debug.Log(keyExists);
+
+            inv.Remove("Apple");
+
+            foreach (var pair in inv)
+            {
+                string key = pair.Key;
+                int value = pair.Value;
+                Debug.Log($"Key: {key} value: {value}");
+            }
+
             throw new System.NotImplementedException();
         }
 
